74a5dd81a36bb8a9da51db47c41a8597199e42fa
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2026-01-06 04:45:28 +0100

    Made Square Drawing Thing

---
+++ desk-pets/WindowRectBarrierSystemScript.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var tempSquare: PackedScene
+
+func _ready() -> void:
+	createLine(Vector2(5,60), Vector2(10, 70))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func createLine(point1: Vector2, point2: Vector2):
+	var middlePoint = (point2 + point1) / 2
+	var instance = tempSquare.instantiate()
+	add_child(instance)
+	
+	instance.global_position = middlePoint
+	instance.scale = point2 - point1
+	pass
--- desk-pets/test.gd
+++ desk-pets/test.gd
@@ -75,26 +75,28 @@ func _process(delta: float) -> void:
 	if windows == null:
 		print("Failed to parse JSON")
 		return
+		
+	print(output);
 	
 	
 	#var ran: bool = false
 	#var screenScale := DisplayServer.screen_get_scale()
-	var dpi := get_window().content_scale_factor
-	var godot_pos := DisplayServer.window_get_position()  # already logical pixels
-
-	var wasFirst := true
-	for w in windows:
-		if wasFirst:
-			wasFirst = false
-			continue  # skip first window if desired
-
-		var instance = tempSquare.instantiate()
-		add_sibling(instance)
-
-		# Convert Windows physical → Godot logical, then relative to Godot window
-		var win_logical := Vector2(w.x, w.y) / dpi
-		instance.global_position = Vector2(win_logical.x - godot_pos.x, win_logical.y - godot_pos.y)
-
-		instance.scale = Vector2(0.1, 0.1)
-		squares.append(instance)
+	#var dpi := get_window().content_scale_factor
+	#var godot_pos := DisplayServer.window_get_position()  # already logical pixels
+#
+	#var wasFirst := true
+	#for w in windows:
+		#if wasFirst:
+			#wasFirst = false
+			#continue  # skip first window if desired
+#
+		#var instance = tempSquare.instantiate()
+		#add_sibling(instance)
+#
+		## Convert Windows physical → Godot logical, then relative to Godot window
+		#var win_logical := Vector2(w.x, w.y) / dpi
+		#instance.global_position = Vector2(win_logical.x - godot_pos.x, win_logical.y - godot_pos.y)
+#
+		#instance.scale = Vector2(0.1, 0.1)
+		#squares.append(instance)
 	pass
