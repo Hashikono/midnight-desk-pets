cee20c06154290eb6dd71793fa0e3c30db564aea
Hashikono <168374339+Hashikono@users.noreply.github.com>
2026-01-06 02:11:32 +0100

    Added the model for the edge outputs...now trying to figure out how the heck this is gonna get calculated

--- edgeDetectionTest.py
+++ edgeDetectionTest.py
@@ -2,14 +2,58 @@
 # translated into GDscript once everything is finalized
 
 #Input from window_helper.exe (or whatever we're using)
-#WINDOW HELPER OUT: [window name, layer #, x1, y1, x2, y2, x3, y3, x4, y4] -> coordinates will be transcribed to be vector2()
-#EDGE DETECTION OUT: [x#,y#] -> length/2 = how many horiontal edges there are
+#WINDOW HELPER OUT: [window name, z layer #, x1, y1, x4, y4] -> coordinates will be transcribed to be vector2()
+#   - z layer # : top layer will be 0 and bottom layer will be the greatest number
+#   - we only need two coordinates for the corners of the rectanglular window (see figure below)
+#EDGE DETECTION OUT: [x1, y1, x2, y2] -> length/4 = how many horiontal edges there are
 
 # 1️⃣---------------2️⃣
 # |                  |
 # |                  |
 # |                  |
 # 3️⃣---------------4️⃣
+
+#placeholder for output retrieved from window_helper.exe
+windowHelperOutput1 = [
+    "window 1", 0, 
+    0, 0, 
+    2, 2, 
+    "window 2", 1, 
+    1, 1, 
+    4, 4
+]
+
+windowHelperOutput2 = [
+    "window 1", 1, 
+    0, 0, 
+    2, 2, 
+    "window 2", 0, 
+    1, 1, 
+    4, 4
+]
+
+#edge detection ouput (coordinates will be appended)
+edgeDetectionOutput = []
+
+#MODEL 1
+# - - - - -   -> 0 0 0 - -
+# - - - - -   -> 0 0 0 1 1
+# - - - - -   -> 0 0 0 1 1
+# - - - - -   -> - 1 1 1 1
+# - - - - -   -> - 1 1 1 1
+#SHOULD OUTPUT: (0,0)-(0,2)  (1,3)-(1,4)
+#   - IF THE COORDINATE PAIRS EQUAL, DON'T APPEND IT TO edgeDetection
+
+#MODEL 1
+# - - - - -   -> 1 1 1 - -
+# - - - - -   -> 1 0 0 0 0
+# - - - - -   -> 1 0 0 0 0
+# - - - - -   -> - 0 0 0 0
+# - - - - -   -> - 0 0 0 0
+#SHOULD OUTPUT: (0,0)-(0,2)  (1,1)-(1,4)
+
+
+
 
 
 
